b9e5a583b98e396835569fd96516312d478bd785
hrushikeshpthube <hrushikeshpthube@gmail.com>
2023-10-31 12:18:32 +0530

    React Fragment

--- src/index.js
+++ src/index.js
@@ -2,19 +2,45 @@ import React, { createElement } from 'react';
 import ReactDOM from 'react-dom';
 
 //display multiple elements using render using array 
-ReactDOM.render(
+/*ReactDOM.render(
 [<h1>Hiii</h1>,
 <p>This is p tag </p>,
 <h2>This is H2 Tag </h2>
 ],
 document.getElementById("root"));
-
-/* 
-//React
-ReactDOM.render(<h1>Hiii</h1>,document.getElementById("root"));
-
-//simple JS
-var h1 = document.createElement("h1");
-h1.innerHTML = "Hello";
-document.getElementById("root").appendChild(h1);
 */
+
+// to display same above using above  using div for Version <v16
+//but below was creating unnecessary additional div tag in HTML 
+// ReactDOM.render(
+//   <div>
+//     <h1>Hiii</h1>
+//     <p>This is p tag </p>
+//     <h2>This is H2 Tag </h2>
+//   </div>,
+//   document.getElementById("root")
+
+// )
+
+//same above using react fragment
+// ReactDOM.render(
+//   <React.Fragment>
+//     <h1>Hiii</h1>
+//     <p>This is p tag </p>
+//     <h2>This is H2 Tag .</h2>
+
+//   </React.Fragment>,
+//   document.getElementById("root")
+// )
+
+
+//same reactfragment can be used as below:
+ReactDOM.render(
+  <>
+    <h1>Hiii</h1>
+    <p>This is p tag </p>
+    <h2>This is H2 Tag .</h2>
+
+  </>,
+  document.getElementById("root")
+)
