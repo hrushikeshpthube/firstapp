e293dae07a11ad0de10f516bb034f4030e33b1e2
hrushikeshpthube <hrushikeshpthube@gmail.com>
2023-11-01 01:30:10 +0530

    learn to Add CSS

--- src/index.js
+++ src/index.js
@@ -1,18 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css"
 
+const name = "Hrushikesh";
 const img1 = "https://picsum.photos/200/300";
-const img2 = "https://picsum.photos/200/300";
-const img3 = "https://picsum.photos/200/300";
+const img2 = "https://picsum.photos/250/300";
+const img3 = "https://picsum.photos/300/300";
 ReactDOM.render(
   <>
-    <h1 contentEditable="true">Hi Demo</h1>
+    <h1 className="heading" >Hi ,I am  {name} .</h1>
     {/* In html contenteditable but in jsx camelCase contentEditable. */}
 
-    <a href="https://adarshwebdeveloperhrushikesh.netlify.app" target="_blanc">
+    <div className="img_div">  
+      <a href="https://adarshwebdeveloperhrushikesh.netlify.app" target="_blanc">
       <img src={img1} alt="RandomImage" />
-      {/* img is self closing tag so use <img />  */}
     </a>
+    <img src={img2} alt="RandomIMg2" />
+    <img src={img3} alt="RandomImg3" />
+    </div>
+
+
 
   </>
   , document.getElementById("root"))
