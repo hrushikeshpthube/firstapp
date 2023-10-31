d3dc83c9fc63e152e52f4a43fafabcf4bc7da9f0
hrushikeshpthube <hrushikeshpthube@gmail.com>
2023-10-31 17:52:36 +0530

    temporary literal challenge try

--- src/index.js
+++ src/index.js
@@ -1,46 +1,18 @@
-import React, { createElement } from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import  ReactDOM from 'react-dom'
 
-//display multiple elements using render using array 
-/*ReactDOM.render(
-[<h1>Hiii</h1>,
-<p>This is p tag </p>,
-<h2>This is H2 Tag </h2>
-],
-document.getElementById("root"));
-*/
-
-// to display same above using above  using div for Version <v16
-//but below was creating unnecessary additional div tag in HTML 
-// ReactDOM.render(
-//   <div>
-//     <h1>Hiii</h1>
-//     <p>This is p tag </p>
-//     <h2>This is H2 Tag </h2>
-//   </div>,
-//   document.getElementById("root")
-
-// )
-
-//same above using react fragment
-// ReactDOM.render(
-//   <React.Fragment>
-//     <h1>Hiii</h1>
-//     <p>This is p tag </p>
-//     <h2>This is H2 Tag .</h2>
-
-//   </React.Fragment>,
-//   document.getElementById("root")
-// )
-
-
-//same reactfragment can be used as below:
+const fname="Hrushikesh"
+const lname="T"
+const d=new Date().toLocaleDateString();
+const t=new Date().toLocaleTimeString();
 ReactDOM.render(
   <>
-    <h1>Hiii</h1>
-    <p>This is p tag </p>
-    <h2>This is H2 Tag .</h2>
-
+    <h1>{`Hello this is ${fname} ${lname}`}</h1>
+    <p>{`Today's date is ${d}`}</p>
+    <p>{`Current Time is ${t}`}</p>
+    
   </>,
   document.getElementById("root")
+
+
 )
