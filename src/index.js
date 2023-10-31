4cf30a2e54e0ee5d471a918b63c43cc82423c077
hrushikeshpthube <hrushikeshpthube@gmail.com>
2023-10-31 12:06:46 +0530

    basics JSX and react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { createElement } from 'react';
+import ReactDOM from 'react-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//display multiple elements using render using array 
+ReactDOM.render(
+[<h1>Hiii</h1>,
+<p>This is p tag </p>,
+<h2>This is H2 Tag </h2>
+],
+document.getElementById("root"));
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+/* 
+//React
+ReactDOM.render(<h1>Hiii</h1>,document.getElementById("root"));
+
+//simple JS
+var h1 = document.createElement("h1");
+h1.innerHTML = "Hello";
+document.getElementById("root").appendChild(h1);
+*/
